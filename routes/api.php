f3f9384df1211b78a62afe2a8e3b02613175e627
Drummer <drummer.ilya@gmail.com>
2022-11-05 17:16:58 +0700

    feat: edit routes for api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\DataController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::apiResource('datas', DataController::class);
