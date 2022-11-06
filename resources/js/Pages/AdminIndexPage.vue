cf5bfcbcfd4baf9d41fb7e3bd328b2144fbda625
Drummer <drummer.ilya@gmail.com>
2022-11-06 21:00:28 +0700

    feat: edit Index page

--- resources/js/Pages/AdminIndexPage.vue
+++ resources/js/Pages/AdminIndexPage.vue
@@ -5,7 +5,9 @@
                 <div v-for="data in datas" :key="data.id">
                     <div class="flex pt-8">
                         <div class="pr-4">{{data.id}}.</div>
-                        <div class="pr-4">{{data.data}}</div>
+                        <div v-for="(dat, index) in data" :key="index" class="pr-4">
+                            {{dat}}
+                        </div>
 
                         <router-link :to="{ name: 'dataEdit', params: { id: data.id } }"
                                      class="font-medium text-blue-600 dark:text-blue-500 hover:text-blue-600 hover:underline">Edit
@@ -18,15 +20,48 @@
     </div>
 </template>
 
-<script setup>
-import useDatas from '@/composables/data.js'
-import { onMounted  } from 'vue'
+<!--<script setup>-->
+<!--import useDatas from '@/composables/data.js'-->
+<!--import { onMounted  } from 'vue'-->
 
 
-const { datas, getDatas } = useDatas()
+<!--const { datas, getDatas } = useDatas()-->
 
-onMounted(getDatas)
+<!--onMounted(getDatas)-->
+<!--</script>-->
+
+<script>
+export default {
+    data() {
+        return {
+            datas: [],
+        }
+    },
+
+
+
+    methods: {
+        loadDatas() {
+            axios
+                .get('/api/datas')
+                .then(res => {
+                    this.datas = res.data.data;
+                    console.log(res.data.data)
+                })
+                .finally(() => {
+                    this.loading = false;
+                });
+        },
+    },
+
+    mounted() {
+        this.loadDatas();
+    }
+
+}
 </script>
+
+
 
 <style scoped>
 
