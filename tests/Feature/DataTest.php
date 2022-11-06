3485c818a814a80058280731d3c492d569c9a101
Drummer <drummer.ilya@gmail.com>
2022-11-06 19:31:14 +0700

    feat: edit test

--- tests/Feature/DataTest.php
+++ tests/Feature/DataTest.php
@@ -4,19 +4,46 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Tests\TestCase;
 
 class DataTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    use RefreshDatabase, WithFaker, WithoutMiddleware;
 
-        $response->assertStatus(200);
+    public $allData;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->allData = [
+            "array" => [
+                1,
+                2,
+                3
+            ],
+            "boolean" => true,
+            "color" => "gold",
+            "null" => null,
+            "number" => 123,
+            "object" => [
+                "a" => "b",
+                "c" => "d"
+            ],
+            "string" => "Hello World"
+        ];
+    }
+
+    public function test_can_post_data()
+    {
+        $this->postJson('/api/datas', $this->allData)
+            ->assertStatus(200);
+    }
+
+    public function test_can_show_data()
+    {
+        $this->get('/api/datas')
+            ->assertStatus(200);
     }
 }
