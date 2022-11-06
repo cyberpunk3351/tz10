c5da32a95c7ab8f46d67f905f1dec3485e11168f
Drummer <drummer.ilya@gmail.com>
2022-11-06 15:39:49 +0700

    feat: add routes

--- resources/js/route.js
+++ resources/js/route.js
@@ -1,13 +1,37 @@
 import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router';
 
 import index from '@/Pages/MainPage.vue'
+import login from '@/Pages/LoginPage.vue'
+import admin from '@/Pages/AdminIndexPage.vue'
+import dataEdit from '@/Pages/AdminDataEditPage.vue'
 
 const routes = [
     {
         path: "/",
         component: index,
         name: "index",
+        meta: { requiresAuth: true },
     },
+    {
+        path: "/login",
+        component: login,
+        name: "login",
+        meta: { requiresVisitor: true }
+    },
+    {
+        path: "/admin",
+        component: admin,
+        name: "admin",
+        props: true,
+        meta: { requiresAuth: true },
+    },
+    {
+        path: '/admin/:id/edit',
+        name: 'dataEdit',
+        component: dataEdit,
+        props: true,
+        meta: { requiresAuth: true },
+    }
 ]
 
 const router = createRouter({
@@ -15,48 +39,23 @@ const router = createRouter({
     routes,
 })
 
-// router.beforeEach((to, from, next) => {
-//     if (to.matched.some(record => record.meta.requiresAuth)) {
-//         const token = localStorage.getItem('access_toke')
-//         // console.log(token)
-//         if (!token) {
-//             next({
-//                 name: 'login',
-//             })
-//         } else {
-//             next()
-//         }
-//     } else if (to.matched.some(record => record.meta.requiresVisitor)) {
-//         const token = localStorage.getItem('access_toke')
-//         if (token) {
-//             next({
-//                 name: 'dashboard',
-//             })
-//         } else {
-//             next()
-//         }
-//     } else {
-//         next()
-//     }
-// })
-
-// router.beforeEach((to, from) => {
-//     // instead of having to check every route record with
-//     // to.matched.some(record => record.meta.requiresAuth)
-//     const token = localStorage.getItem('access_token')
-//     if (to.meta.requiresAuth && !token) {
-//       // this route requires auth, check if logged in
-//       // if not, redirect to login page.
-//       return {
-//         name: 'login',
-//         // save the location we were at to come back later
-//       }
-//     } else if(to.meta.requiresVisitor && token) {
-//         return {
-//             name: 'dashboard',
-//             // save the location we were at to come back later
-//           }
-//     }
-//   })
+router.beforeEach((to, from) => {
+    // instead of having to check every route record with
+    // to.matched.some(record => record.meta.requiresAuth)
+    const token = localStorage.getItem('access_token')
+    if (to.meta.requiresAuth && !token) {
+        // this route requires auth, check if logged in
+        // if not, redirect to login page.
+        return {
+            name: 'login',
+            // save the location we were at to come back later
+        }
+    } else if(to.meta.requiresVisitor && token) {
+        return {
+            name: 'index',
+            // save the location we were at to come back later
+        }
+    }
+})
 
 export default router;
